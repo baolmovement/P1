15e95b3471ece9e26917360f383fa6382e5d94d2
Andrew Bao <bao.andrew@gmail.com>
2018-05-24 10:34:35 -0700

    finished logic

--- js/main.js
+++ js/main.js
@@ -17,48 +17,40 @@ function switchTurns() {
         currentPlayer = player1
     }
 }
+function highScore(){
+    if(player1.score>player2.score===true){
+        return "Player 1"
+    } else if(player2.score>player1.score===true){
+        return "Player 2"
+    }
+}   
+function lowScore(){
+    if(player1.score<player2.score===true){
+        return "Player 1"
+    } else if(player2.score<player1.score===true){
+        return "Player 2"
+    } 
+}
+
+
+
 //Global Announcement Variables 
 var $announcement=$("#Announcement")
+
+
 // Global Gameboard Variables
 var $gameboard=$(".gameboard") 
 var $m1=$(".m1")  
 var $button=$("button") 
 var $h3=$("h3")    
 var $rules=$(".rules")  
-var music = new Audio("audio/FrankSinatra-LoveAndMarriage(+lyrics)[HD].mp3") 
-// var gameBoard = [
-//     { imgPath: "images/anniversary.png", pointValue: 500 }, 
-//     { imgPath: "images/badbusiness.png", pointValue: -2 },
-//     { imgPath: "images/balenciaga.png", pointValue: -1 },
-//     { imgPath: "images/brokenleg.png", pointValue: -2 },
-//     { imgPath: "images/burglar.png", pointValue: -2 },
-//     { imgPath: "images/cheating.png", pointValue: -500 },
-//     { imgPath: "images/doodle.png", pointValue: 3 },
-//     { imgPath: "images/fenderbender.png", pointValue: -2 },
-//     { imgPath: "images/grandma.png", pointValue: 1 },
-//     { imgPath: "images/hacker.png", pointValue: -2 },
-//     { imgPath: "images/hybrid.png", pointValue: 2 },
-//     { imgPath: "images/inherritance.png", pointValue: 3 },
-//     { imgPath: "images/joboffer.png", pointValue: 3 },
-//     { imgPath: "images/letgo.png", pointValue: -3 },
-//     { imgPath: "images/lotto.png", pointValue: 2 },
-//     { imgPath: "images/metermaid.png", pointValue: -1 },
-//     { imgPath: "images/movie.png", pointValue: 2 },
-//     { imgPath: "images/nigerianprincess.png", pointValue: -1 },
-//     { imgPath: "images/raffle.png", pointValue: 1 },
-//     { imgPath: "images/raise.png", pointValue: 2 },
-//     { imgPath: "images/taxaccountant.png", pointValue: 3 },
-//     { imgPath: "images/trump.png", pointValue: -1 },
-//     { imgPath: "images/vandal.png", pointValue: -1 },
-//     { imgPath: "images/vote.png", pointValue: -1 }, 
-//     { imgPath: "images/cheating.png", pointValue: -500 } 
-// ]     
-
+var music = new Audio("audio/FrankSinatra-LoveAndMarriage(+lyrics)[HD].mp3")    
 
 
 //Automatic Settings
 $gameboard.slideUp()  
 music.loop = true 
+
 
 //Game Start
 function startGame() {
@@ -74,7 +66,6 @@ function startGame() {
         $(".gameboard img").animate({opacity: 0})
     },4000) 
 }
-
 function restartGame() {
     $announcement.text("Game Announcement")
     music.currentTime = 0   
@@ -86,19 +77,23 @@ function restartGame() {
     player1.scoreBoard.text(player1.score) 
     player2.scoreBoard.text(player2.score) 
 }
-
 $button.on("click", startGame) 
 
-function checkScore() {
+function checkScore() { 
     if(player1.score > 399 || player2.score > 399) {
-        $announcement.text("Y'all make a good couple, but Player 1 needs to step up")
+        $announcement.text("Y'all make a good couple, but"+" "+lowScore()+" "+"needs to step up")
         setTimeout(function() {
             $('.gameboard img').remove()
             restartGame()
-        }, 3000)   
+        }, 5000)   
+    }else if(player1.score < -399 || player2.score < -399){
+        $announcement.text("Doomed relationship, but"+" "+highScore()+" "+" will do all right") 
+        setTimeout(function() {
+            $('.gameboard img').remove()
+            restartGame()
+        }, 5000) 
     }
 }
-
 function rando(){
     var gameBoard = [
         { imgPath: "images/anniversary.png", pointValue: 500 }, 
@@ -137,6 +132,7 @@ function rando(){
         $gameboard.append($img)
     }
 } 
+
 
 // Gameplay & Scoring
 $gameboard.on("click","img",function(){
