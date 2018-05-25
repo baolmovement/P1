4093c2dfceb1fbccf72e191e6cafec7cff9e75bf
Andrew Bao <bao.andrew@gmail.com>
2018-05-24 21:40:36 -0700

    fixed bugs and more styling

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ function lowScore(){
 var $announcement=$("#Announcement")
 
 
+
 // Global Gameboard Variables
 var $gameboard=$(".gameboard") 
 var $m1=$(".m1")  
@@ -47,13 +48,19 @@ var $rules=$(".rules")
 var music = new Audio("audio/FrankSinatra-LoveAndMarriage(+lyrics)[HD].mp3")    
 
 
+
 //Automatic Settings
 $gameboard.slideUp()  
 music.loop = true 
 
 
-//Game Start
-function startGame() {
+
+//Game Start 
+$button.on("click", startGame) 
+function startGame() {    
+    $("header").slideUp() 
+    $gameboard.children().prop("disabled",true)
+    $button.prop("disabled",true)
     $announcement.text("Game Announcement")
     music.currentTime = 0   
     music.play()
@@ -64,63 +71,35 @@ function startGame() {
     rando(); 
     setTimeout(function(){ 
         $(".gameboard img").animate({opacity: 0})
-    },4000) 
-}
-function restartGame() {
-    $announcement.text("Game Announcement")
-    music.currentTime = 0   
-    music.play()
-    $rules.fadeIn()
-    $gameboard.slideUp()
-    player1.score = 0
-    player2.score = 0 
-    player1.scoreBoard.text(player1.score) 
-    player2.scoreBoard.text(player2.score) 
-}
-$button.on("click", startGame) 
-
-function checkScore() { 
-    if(player1.score > 399 || player2.score > 399) {
-        $announcement.text("Y'all make a good couple, but"+" "+lowScore()+" "+"needs to step up")
-        setTimeout(function() {
-            $('.gameboard img').remove()
-            restartGame()
-        }, 5000)   
-    }else if(player1.score < -399 || player2.score < -399){
-        $announcement.text("Doomed relationship, but"+" "+highScore()+" "+" will do all right") 
-        setTimeout(function() {
-            $('.gameboard img').remove()
-            restartGame()
-        }, 5000) 
-    }
+    },5000)  
 }
 function rando(){
     var gameBoard = [
-        { imgPath: "images/anniversary.png", pointValue: 500 }, 
-        { imgPath: "images/badbusiness.png", pointValue: -2 },
-        { imgPath: "images/balenciaga.png", pointValue: -1 },
-        { imgPath: "images/brokenleg.png", pointValue: -2 },
-        { imgPath: "images/burglar.png", pointValue: -2 },
+        { imgPath: "images/anniversary.png", pointValue: 50 }, 
+        { imgPath: "images/badger3.png", pointValue: -2 },
+        { imgPath: "images/balenciaga2.png", pointValue: -1 },
+        { imgPath: "images/brokenleg3.png", pointValue: -2 },
+        { imgPath: "images/burglar2.png", pointValue: -2 },
         { imgPath: "images/cheating.png", pointValue: -500 },
-        { imgPath: "images/doodle.png", pointValue: 3 },
-        { imgPath: "images/fenderbender.png", pointValue: -2 },
-        { imgPath: "images/grandma.png", pointValue: 1 },
-        { imgPath: "images/hacker.png", pointValue: -2 },
-        { imgPath: "images/hybrid.png", pointValue: 2 },
-        { imgPath: "images/inherritance.png", pointValue: 3 },
-        { imgPath: "images/joboffer.png", pointValue: 3 },
-        { imgPath: "images/letgo.png", pointValue: -3 },
-        { imgPath: "images/lotto.png", pointValue: 2 },
-        { imgPath: "images/metermaid.png", pointValue: -1 },
-        { imgPath: "images/movie.png", pointValue: 2 },
-        { imgPath: "images/nigerianprincess.png", pointValue: -1 },
-        { imgPath: "images/raffle.png", pointValue: 1 },
-        { imgPath: "images/raise.png", pointValue: 2 },
-        { imgPath: "images/taxaccountant.png", pointValue: 3 },
-        { imgPath: "images/trump.png", pointValue: -1 },
-        { imgPath: "images/vandal.png", pointValue: -1 },
-        { imgPath: "images/vote.png", pointValue: -1 }, 
-        { imgPath: "images/cheating.png", pointValue: -500 } 
+        { imgPath: "images/doodle2.png", pointValue: 3 },
+        { imgPath: "images/fenderbender2.png", pointValue: -2 },
+        { imgPath: "images/grandma2.png", pointValue: 1 },
+        { imgPath: "images/hacked2.png", pointValue: -2 },
+        { imgPath: "images/hybrid2.png", pointValue: 2 },
+        { imgPath: "images/inherritance2.png", pointValue: 3 },
+        { imgPath: "images/joboffer2.png", pointValue: 3 },
+        { imgPath: "images/fired.png", pointValue: -3 },
+        { imgPath: "images/lotto2.png", pointValue: 2 },
+        { imgPath: "images/metermaid2.png", pointValue: -1 },
+        { imgPath: "images/movie2.png", pointValue: 2 },
+        { imgPath: "images/nigerianprincess2.png", pointValue: -1 },
+        { imgPath: "images/raffle2.png", pointValue: 1 },
+        { imgPath: "images/raise2.png", pointValue: 2 },
+        { imgPath: "images/taxaccountant2.png", pointValue: 3 },
+        { imgPath: "images/trump2.png", pointValue: -1 },
+        { imgPath: "images/vandal1.png", pointValue: -1 },
+        { imgPath: "images/vote1.png", pointValue: -1 }, 
+        { imgPath: "images/cheating.png", pointValue: -50 } 
     ]   
     var shuffledDeck=[] 
     for(var i=0;i<25;i++){  
@@ -134,6 +113,7 @@ function rando(){
 } 
 
 
+
 // Gameplay & Scoring
 $gameboard.on("click","img",function(){
     $(this).animate({opacity: 1})   
@@ -141,7 +121,37 @@ $gameboard.on("click","img",function(){
     currentPlayer.scoreBoard.text(currentPlayer.score)
     switchTurns()
     checkScore()
-})   
+})    
+function checkScore() {   
+    if(player1.score > 49 || player2.score > 49) { 
+        $gameboard.children().prop("disabled",true)
+        $announcement.text("Y'all make a good couple, but"+" "+lowScore()+" "+"needs to step up")
+        setTimeout(function() {
+            $('.gameboard img').remove()
+            restartGame()
+        }, 5000)   
+    }else if(player1.score < -49 || player2.score < -49){  
+        $gameboard.children().prop("disabled",true)
+        $announcement.text("Doomed relationship, but"+" "+highScore()+" "+" will do all right") 
+        setTimeout(function() {
+            $('.gameboard img').remove()
+            restartGame()  
+        }, 5000) 
+    }   
+}
+function restartGame() { 
+    $("header").slideDown()
+    $announcement.text("Game Announcement")
+    music.currentTime = 0   
+    music.play()
+    $rules.fadeIn()
+    $gameboard.slideUp()
+    player1.score = 0
+    player2.score = 0 
+    player1.scoreBoard.text(player1.score) 
+    player2.scoreBoard.text(player2.score)   
+    $button.prop("disabled",false) 
+}
 
 
 
